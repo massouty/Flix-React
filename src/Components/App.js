345c77713fb5d11cd9b8c1c7d375fa0c38a1a155
massouty <massouty@outlook.com>
2022-10-01 00:58:59 +0200

    changed some codes
    if

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,7 +3,7 @@
 import React ,{useEffect, useState} from  "react";
 import MovieBox from "./MovieBox/MovieBox";
 import './App.css';
-import {Navbar} from 'react-bootstrap';
+import {Navbar,Container,Nav,Form,FormControl,Button} from 'react-bootstrap';
 
 
 const API_URL="https://api.themoviedb.org/3/movie/popular?api_key=3b885affc5cf1baf5603690472bf4c6e";
@@ -21,20 +21,37 @@ function App() {
 
   return (
     <>
+    
     <Navbar bg="dark" expand="lg" variant="dark">
-    <div className="container">
+        
+        <Container fluid>
+            <Navbar.Brand href='' >Login</Navbar.Brand>
+             <Navbar.Brand href=''>Register</Navbar.Brand>
+             <Navbar.Toggle  aria-controls='navbarScroll'>
+                <Navbar.Collapse  id="navbarScroll">
+                <Nav  className="me-auto my=2 my=lg=3" style={{maxHeight:'400px'}} navbarScroll>
+                    
+                </Nav>
+ </Navbar.Collapse>
+</Navbar.Toggle>
+</Container>
+    </Navbar>
+    <Form className="d-flex bg=primary">
+                <FormControl type="search" placeholder="Movie Search"
+                className="me-2" aria-label="search"
+                name=""></FormControl>
+                <Button variant="primary" type="submit">Search</Button>
+
+             </Form>
+ 
+     <div className="container">
      <div className="grid">
       {movies.map((movieReq)=>
       <MovieBox key={movieReq.id}{...movieReq}/>)}
      </div>
-    </div>
-
-
-
-    </Navbar>
+     </div>
     </>
   );
-   
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './Components/App';
 
+import Footer from './Components/Footer';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Footer/>
   </React.StrictMode>
 );
 
