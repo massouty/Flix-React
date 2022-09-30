416603b75c9b2d4fed2d09b70f6a5be0816d9df8
massouty <massouty@outlook.com>
2022-10-01 00:57:45 +0200

    added new files

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+class Footer extends React.Component{
+    render(){
+        return(
+            <div className="foot" >
+            <ul className="list-group">
+                <li className="list-group-item"><a href='https://www.facebook.com/massouty'>Facebook</a></li>
+                <li className="list-group-item"><a href='https://www.youtube.com/channel/UCyawU6msveC4s7TgqeXmCUA'>Youtube</a></li>
+                <li className="list-group-item">Contact us : Email:massouty@outlook.com</li>
+
+            </ul>
+            </div>
+
+        )
+    }
+}
+
+export default Footer;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+ class Header extends React.Component{
+    render(){
+        return(
+            <div className="navbar bg-secondary">
+             
+                
+            </div>
+        )
+    }
+}
+
+export default Header;
+
+
