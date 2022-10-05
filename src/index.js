b09b7b99576d1e44b7b0333a48b24cdf8941d602
massouty <massouty@outlook.com>
2022-10-05 18:34:07 +0200

    do not nothing

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import MainView from './Components/MainView/MainView';
 
+
 import Footer from './Components/Footer';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+
     <MainView />
+
     <Footer/>
   </React.StrictMode>
 );
